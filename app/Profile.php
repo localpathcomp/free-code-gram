69fe11693327f91489b3899a31af6f12159b7834
Garrick Crouch <services@localpathcomputing.com>
2020-05-03 00:03:25 -0500

    update user profiles

--- app/Profile.php
+++ app/Profile.php
@@ -6,8 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
+
+    protected $guarded = [];
+
+    public function profileImage()
+    {
+        $imagePath = ($this->image) ? $this->image : 'profile/4tddxQG8DJ6ELSDIo4OgM0NzOSrNkpFE3lMCEBDe.png';
+
+        return '/storage/' . $imagePath;
+    }
+
     public function user()
     {
         return $this->belongsTo(User::class);
     }
+
 }
